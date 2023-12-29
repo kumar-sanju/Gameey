2ead5f68b3b0e3b341386aecb82ddc479356487d
Sanjay <sanjay.mahapatra@muvi.com>
2023-12-29 20:11:44 +0530

    ads implimented successfully

--- app/src/main/java/com/smart/gameey/StartActivity.java
+++ app/src/main/java/com/smart/gameey/StartActivity.java
@@ -1,15 +1,29 @@
 package com.smart.gameey;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.smart.gameey.sankeGame.SnakeActivity;
 import com.smart.gameey.spaceShooter.Start;
+
+import java.util.HashMap;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -18,9 +32,76 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        final FirebaseRemoteConfig mFirebaseRemoteConfig;
+
+        HashMap<String, Object> defaultsRate = new HashMap<>();
+        defaultsRate.put("gameey_version_code", String.valueOf(getVersionCode()));
+
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(10) // change to 3600 on published app
+                .build();
+
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+        mFirebaseRemoteConfig.setDefaultsAsync(defaultsRate);
+
+        mFirebaseRemoteConfig.fetchAndActivate().addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+            @Override
+            public void onComplete(@NonNull Task<Boolean> task) {
+                if (task.isSuccessful()) {
+                    final String gameey_version_code = mFirebaseRemoteConfig.getString("gameey_version_code");
+
+                    if(Integer.parseInt(gameey_version_code) > getVersionCode())
+                        showTheDialog(getApplication().getPackageName(), gameey_version_code );
+                }
+                else Log.e("MYLOG", "mFirebaseRemoteConfig.fetchAndActivate() NOT Successful");
+
+            }
+        });
+
+
         AdView adView = (AdView)findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
+    }
+
+    private PackageInfo pInfo;
+    private int getVersionCode() {
+        pInfo = null;
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.i("sanju", "NameNotFoundException: "+e.getMessage());
+        }
+        return pInfo.versionCode;
+    }
+
+    private void showTheDialog(final String appPackageName, String versionFromRemoteConfig) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final View customLayout = getLayoutInflater().inflate(R.layout.update_alert, null);
+        builder.setView(customLayout);
+        TextView updateText = customLayout.findViewById(R.id.text_dialog);
+        Button updateBtn = customLayout.findViewById(R.id.updateBtn);
+//        updateText.setText("This current version "+getVersionCode()+" is obsolete, please update the new version: " + versionFromRemoteConfig);
+//        updateText.setText("This app version is obsolete, update the app now.");
+        updateText.setText("This current version is obsolete, please update the new version");
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.setCancelable(false);
+
     }
 
     public void spaceShooter(View view) {
