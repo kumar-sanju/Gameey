c8af525c5965dea75d0c9f35ae909cd07fe2a1de
Sanjay <sanjay.mahapatra@muvi.com>
2023-12-29 23:46:31 +0530

    share rate added

--- app/src/main/java/com/smart/gameey/StartActivity.java
+++ app/src/main/java/com/smart/gameey/StartActivity.java
@@ -3,13 +3,17 @@ package com.smart.gameey;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +22,9 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.smart.gameey.sankeGame.SnakeActivity;
@@ -26,6 +33,9 @@ import com.smart.gameey.spaceShooter.Start;
 import java.util.HashMap;
 
 public class StartActivity extends AppCompatActivity {
+
+    FloatingActionButton addIcon, shareApp, rateApp, copyAppLink;
+    boolean fabVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +73,107 @@ public class StartActivity extends AppCompatActivity {
         AdView adView = (AdView)findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
+
+        addIcon = findViewById(R.id.addIcon);
+        shareApp = findViewById(R.id.shareApp);
+        rateApp = findViewById(R.id.rateApp);
+        copyAppLink = findViewById(R.id.copyAppLink);
+        addIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!fabVisible) {
+                    rateApp.setVisibility(View.VISIBLE);
+                    shareApp.setVisibility(View.VISIBLE);
+
+                    addIcon.setImageDrawable(getResources().getDrawable(R.drawable.ic_close));
+                    fabVisible = true;
+                } else {
+                    rateApp.setVisibility(View.GONE);
+                    shareApp.setVisibility(View.GONE);
+
+                    addIcon.setImageDrawable(getResources().getDrawable(R.drawable.ic_add));
+                    fabVisible = false;
+                }
+            }
+        });
+
+        rateApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent rateIntent = rateIntentForUrl("market://details");
+                    startActivity(rateIntent);
+                }
+                catch (ActivityNotFoundException e) {
+                    Intent rateIntent = rateIntentForUrl("https://play.google.com/store/apps/details");
+                    startActivity(rateIntent);
+                }
+            }
+        });
+
+        shareApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Download this amazing Gameey App ");
+                    String shareMessage= "\nfrom play store\n\n";
+                    shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                    startActivity(Intent.createChooser(shareIntent, "choose one"));
+                } catch(Exception e) {
+                    //e.toString();
+                }
+            }
+        });
+
+    }
+
+    private Intent rateIntentForUrl(String url)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("%s?id=%s", url, getPackageName())));
+        int flags = Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK;
+        if (Build.VERSION.SDK_INT >= 21)
+        {
+            flags |= Intent.FLAG_ACTIVITY_NEW_DOCUMENT;
+        }
+        else
+        {
+            //noinspection deprecation
+            flags |= Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET;
+        }
+        intent.addFlags(flags);
+        return intent;
+    }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.inflate(R.menu.expandable_menu);
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_item1:
+                        showMessage("Item 1 Clicked");
+                        return true;
+
+                    case R.id.action_item2:
+                        showMessage("Item 2 Clicked");
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        popupMenu.show();
+    }
+
+    private void showMessage(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
     }
 
     private PackageInfo pInfo;
